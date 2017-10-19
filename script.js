080f592863d013a14e6729019a8a4c50cd049778
Saint. Clever <parchment@gmx.com>
2017-10-19 01:19:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// locate the like button and add a unique id.
+// In this case my id is 'like'
+// add the click method to your id called like: like.click();
+
+for (let i = 0; i < 1; i++) {
+	setInterval(function() {
+		// like.click();
+		console.log('like.click();');
+	}, 1000);
+}
